9e5922c3f7dab07e6d4d9eafb4e45e705cdded7c
Stiven Garzon <usg210802@outlook.com>
2025-06-27 19:46:08 -0500

    fix: mejora en la validación con Zod

--- backend/schemas/validarDatos.js
+++ backend/schemas/validarDatos.js
@@ -3,18 +3,20 @@ import { z } from 'zod';
 const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[.!@#$%^&*])[\S]{8,16}$/;
 
 const userSchema = z.object({
-  name: z.string()
-    .min(6, { message: 'error1' }),
+  
+   name: z.string()
+    .min(12, { message: 'Se debe incluir al menos el primer nombre y el primer apellido' })
+    .regex(/^[^0-9]*$/, { message: 'El nombre no debe contener números' }),
 
   email: z.string().email({
-    message: 'El correo debe ser un correo valido'
+    message: 'El correo debe ser institucional "@ucundinamarca.edu.co"'
   }).refine(
     (email) => email.endsWith('@ucundinamarca.edu.co'), {
-      message: 'error2'
+      message: 'El correo debe ser institucional "@ucundinamarca.edu.co"'
     }
   ),
 
-  password: z.string().regex(regex, { message: 'error3' }),
+  password: z.string().regex(regex, { message: 'La contraseña debe tener entre 8 y 16 caracteres, con mayúsculas, minúsculas, números y un carácter especial' }),
 
   role: z.enum(['student', 'teacher', 'admin']).optional().default('student')
 
